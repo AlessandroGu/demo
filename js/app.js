74842ad0e3be83ff17b90316ffb8d7c9cf625d6c
pipixia <pipixia@ppx.com>
2017-07-18 10:34:53 +0800

    05-修改任务

--- js/app.js
+++ js/app.js
@@ -55,5 +55,25 @@
 			}
 		};
 
+		// 4 修改任务
+		// 思路：双击任务元素，给当前项添加 editing 类
+
+		// 给 $scope 添加一个 editingId ，用来记录当前正在修改项的id值，默认值：-1
+		// 双击某一个任务，将当前项的id 赋值为 editingId，此时引起了数据的变化，
+		// 页面中与 editingId 相关的指令都会被重新计算，那么 editingId === todo.id
+		// 当前双击的这一项id就与 editingId 相同了，那么当前项就会添加类
+		vm.editingId = -1;
+		vm.edit = function(id) {
+			vm.editingId = id;
+		};
+		// 编辑文本框通过 todo.name 与数据双向绑定，当我们在视图中修改了任务名称以后
+		// 然后，数据会自动发生变化。
+		// 敲回车，执行 vm.editingId = -1 ，数据发生变化，重新计算 editingId === todo.id
+		// 此时，所有任务项的id 与 editingId 都不相同，所以，元素都移除这个类
+		vm.editSave = function() {
+			vm.editingId = -1;
+		};
+
+
 	}
 })(angular);
